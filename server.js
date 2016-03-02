f87134d345ce6f4fafd5039ac637a1bec712a35f
jeandrades <jeandrades@nubise.com>
2016-03-02 09:58:42 -0430

    include bootstrap

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var express = require('express');
 app.use(express.static(__dirname+'/img'));
+app.use(express.static(__dirname+'/bootstrap'));
 server.listen(8899);
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
