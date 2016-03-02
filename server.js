e5496eb4680449fdcc7ba99361fb0fbe6d204637
jeandrades <jeandrades@nubise.com>
2016-03-02 15:07:01 -0430

    mejoras varias

--- server.js
+++ server.js
@@ -20,10 +20,8 @@ io.on('connection', function (socket) {
 });
 app.post('/notificaciones', function(request, respond) {
     request.on('data', function(data) {
-		console.log('data recibida'+data);
         var body = JSON.parse(data);
         body.fecha = Date();
-        console.log('data recibida 2:'+body);
 		io.sockets.emit('news', body);
          var mydatos =  {
                 color:body.color,
