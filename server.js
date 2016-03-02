373bca144219ffd986209e1c4ed864c9caaea741
JRINC <junior9482@gmail.com>
2016-03-02 16:43:14 -0430

    Crear avatar.
    Crear avatar.

--- server.js
+++ server.js
@@ -3,12 +3,15 @@ var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var express = require('express');
 app.use(express.static(__dirname+'/img'));
+app.use(express.static(__dirname+'/scripts'));
+app.use(express.static(__dirname+'/styles'));
 server.listen(8899);
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 io.on('connection', function (socket) {
   socket.emit('news', { data: 'Server up :D',color:'aa41d3' });
+  socket.emit('news', { user: 'Server', avatar: 'unnamed.jpg', data: 'Server up :D',color:'aa41d3' });
   socket.on('my other event', function (data) {});
 });
 app.post('/notificaciones', function(request, respond) {
