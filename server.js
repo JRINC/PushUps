47f62d9fe8b49b5091327aaf56ad3b089bafe27f
jeandrades <jeandrades@nubise.com>
2016-03-02 11:49:30 -0430

    Logs de mensajes

--- server.js
+++ server.js
@@ -2,24 +2,47 @@ var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var express = require('express');
+var fs = require('fs');
 app.use(express.static(__dirname+'/img'));
 app.use(express.static(__dirname+'/bootstrap'));
+app.use(express.static(__dirname+'/temp'));
 server.listen(8899);
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
+app.get('/json', function (req, res) {
+  var configFile = fs.readFileSync('temp/backup.json');
+  res.send(configFile);
+});
 io.on('connection', function (socket) {
-  socket.emit('news', { data: 'Server up :D',color:'aa41d3' });
+  socket.emit('news', { data: 'Server up :D',color:'aa41d3',fecha:Date() });
   socket.on('my other event', function (data) {});
 });
 app.post('/notificaciones', function(request, respond) {
     request.on('data', function(data) {
 		console.log('data recibida'+data);
         var body = JSON.parse(data);
+        body.fecha = Date();
         console.log('data recibida 2:'+body);
 		io.sockets.emit('news', body);
+         var mydatos =  {
+                color:body.color,
+                mensaje:body.data,
+                fecha: body.fecha
+            };
+        appendObject(mydatos);
 		respond.end();
     })
 });
+
+
+function appendObject(obj)
+{
+  var configFile = fs.readFileSync('temp/backup.json');
+  var config = JSON.parse(configFile);
+  config['Backup'].push(obj);
+  var configJSON = JSON.stringify(config);
+  fs.writeFileSync('temp/backup.json', configJSON);
+}
 
 
